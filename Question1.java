8935e442c6672f8e8bedb56af46d9f042396f40d
shristi-bhushan <shristibhushan2001@gmail.com>
2023-05-20 20:18:07 +0530

    question added

---
+++ Question1.java
@@ -0,0 +1,5 @@
+//Write a program to find the middle character of a palindrome formed by the following list. Examples:
+//                     [a,m,m, a,d]      -> madam       	return: d
+//                     [i,i,e,d,e,r,d,r,v] -> redivider		return: v
+
+
